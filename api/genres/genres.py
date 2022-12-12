c9eab4913abe83daadb68d077010eaf69928c89e
gonz <gonzasestopal@gmail.com>
2022-12-12 11:37:50 -0600

    feat: genre by id

--- api/genres/genres.py
+++ api/genres/genres.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 
 from src.models import Genre
@@ -10,3 +10,12 @@ genres_router = APIRouter()
 async def list_all_genres():
     genres = [genre.as_dict for genre in Genre.query().all()]
     return JSONResponse(content=genres)
+
+@genres_router.get("/genres/{genre_id}")
+async def get_genre(genre_id):
+    genre = Genre.query().filter(Genre.id == genre_id).first()
+
+    if not genre:
+        return HTTPException(status_code=404)
+
+    return JSONResponse(content=genre.as_dict)
