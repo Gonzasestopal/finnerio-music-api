428f99e59ea4bc52b0730c66e4c2a134ab7ed607
gonz <gonzasestopal@gmail.com>
2022-12-12 16:20:30 -0600

    feat: fetch songs by album

--- api/genres/albums.py
+++ api/genres/albums.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 
-from src.models import  Song
+from src.models import  Song, Album
 
 albums_router = APIRouter()
 
 
 @albums_router.get("/albums/{album_id}")
 async def get_songs_by_album(album_id):
-    songs = Song.query().filter(Song.album_id == album_id).all()
+    songs = Song.query().join(Album, Album.id == Song.album_id).filter(Song.album_id == album_id).all()
 
     if not songs:
         return HTTPException(status_code=404)
